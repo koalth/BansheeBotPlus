dc13cb620f561f00ffc933a61000f9ded6212e5f
koalth <koalrube@gmail.com>
2024-09-13 17:52:52 -0400

    updated some stuff'

--- cogs/server.py
+++ cogs/server.py
@@ -1,31 +1,32 @@
 import discord
 from discord.utils import get
+from discord.ext import commands
 
 from core import Cog, Context, ServerModel
 
 
 class Server(Cog):
     """Commands related to server settings"""
-    
+
     async def show(self, ctx: Context):
         """Show the settings for this server."""
         pass
-    
+
     async def set_manager_role(self, ctx: Context, role: discord.Role):
         """Set the manager role"""
         pass
-    
+
     async def set_raider_role(self, ctx: Context, role: discord.Role):
         """Set the raider role"""
         pass
-    
+
     guild = discord.SlashCommandGroup(
         "guild",
         "Commands related to the World of Warcraft guild.",
         guild_only=True,
         default_member_permissions=discord.Permissions(manage_guild=True)
     )
-    
+
     @guild.command(name="set")
     @discord.option("name", description="The name of the guild in World of Warcraft")
     @discord.option("realm", description="The realm of the guild in World of Warcraft")
@@ -33,8 +34,13 @@ class Server(Cog):
     async def set_wow_guild(self, ctx: Context, name: str, realm: str, region: str):
         """Sets the World of Warcraft guild"""
         pass
-        
+
     @guild.command(name="show")
     async def show_guild(self, ctx: Context):
         """Show the World of Warcraft guild"""
+        pass
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, ctx: Context, guild: discord.Guild):
+        """Creates the Server model when bot joins the server"""
         pass
--- core/bot.py
+++ core/bot.py
@@ -8,7 +8,7 @@ from .context import Context
 from .models import ServerModel, CharacterModel
 
 class BansheeBot(commands.Bot):
-    
+
     def __init__(self) -> None:
         super().__init__(
             intents=discord.Intents(
@@ -18,27 +18,32 @@ class BansheeBot(commands.Bot):
                 guilds=True
             )
         )
-        
+
     async def setup_tortoise(self) -> None:
         await Tortoise.init(
             db_url="sqlite://data/database.db", modules={"models": ["core.models"]}
         )
         await Tortoise.generate_schemas()
-        
+
     async def start(self, token: str, *, reconnect: bool = True) -> None:
         await self.setup_tortoise()
         return await super().start(token, reconnect=reconnect)
-    
+
     async def close(self) -> None:
         await Tortoise.close_connections()
         return await super().close()
-    
+
+    async def get_application_context(
+        self, interaction: discord.Interaction
+    ) -> Context:
+        return Context(self, interaction)
+
     def run(self, debug: bool = False, cogs: list[str] | None = None) -> None:
         if debug:
             return super().run(getenv("DEBUG_TOKEN", getenv("TOKEN")))
-        
+
         super().run(getenv("TOKEN"))
-        
+
     async def on_application_command_error(self, ctx: discord.ApplicationContext, error: Exception) -> None:
         await ctx.respond(
             embed=discord.Embed(
